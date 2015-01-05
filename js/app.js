2739395ff7dccc3ec774fd9207b053889eabe06e
Michael LoNigro <mvlonigro@gmail.com>
2015-01-04 23:30:30 -0700

    Added timer to the top of the page
    Timer added with start, stop, and reset buttons.

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@
 
 var App = angular.module('todo-timer', ['ui.sortable']);
 
-App.controller('toDoCtrl', ['$scope', function ($scope) {
-	$scope.init = function () {
+App.controller('toDoCtrl', ['$scope', '$timeout', function($scope, $timeout) {
+	$scope.init = function() {
 		$scope.model = [{
 			'task': 'This is a test task',
 			'done': false
@@ -13,11 +13,17 @@ App.controller('toDoCtrl', ['$scope', function ($scope) {
 			'done': false
 		}];
 
+		// Global time is stored in seconds
+		$scope.globalTimeNum = 0;
+		$scope.globalTime = formatTime($scope.globalTimeNum);
+
 		$scope.show = "All";
 		$scope.currentShow = 0;
 	};
 
-	$scope.addTask = function () {
+
+	// Task functions
+	$scope.addTask = function() {
 		// Add to the end of the array model
 		if ($scope.newTask.length > 0) {
 			$scope.model.push({ task: $scope.newTask, done: false });
@@ -25,60 +31,79 @@ App.controller('toDoCtrl', ['$scope', function ($scope) {
 		$scope.newTask= '';
 	};
 
-	$scope.deleteTask = function (ind) {
+	$scope.deleteTask = function(ind) {
 		$scope.model.splice(ind, 1);
 	};
 
-	$scope.completeTask = function (ind) {
+	$scope.completeTask = function(ind) {
 		$scope.model[ind].done = !$scope.model[ind].done;
 	};
 
-	$scope.clearCompleteTasks = function () {
+	$scope.clearCompleteTasks = function() {
 		$scope.model = $scope.model.filter(function(item) { return !item.done; });
 	};
 
-  $scope.sortableOptions = {
-    handle: ".fa-reorder"
-  };
+
+	// Timer interface functions
+	$scope.startTimer = function() {
+		incrementTimer();
+	};
+
+	$scope.stopTimer = function() {
+		$timeout.cancel($scope.timeout);
+	};
+
+	$scope.resetTimer = function() {
+		adjustTime(0);
+	};
+
+
+	// Timer helper functions
+	function adjustTime(newTime) {
+		$scope.globalTimeNum = newTime;
+		$scope.globalTime = formatTime($scope.globalTimeNum);
+	}
+
+	function incrementTimer() {
+		var newTime = $scope.globalTimeNum + 1;
+		adjustTime(newTime);
+		$scope.timeout = $timeout(incrementTimer, 1000);
+	}
+
+	function formatTime(time) {
+		// Format seconds into hours:minutes:seconds format
+		var hours = Math.floor(time / 3600);
+		var minutes = Math.floor((time - (hours * 3600)) / 60);
+		var seconds = time - (hours * 3600) - (minutes * 60);
+
+		// Add leading zeros if necessary
+		if (hours < 10) {
+			hours = '0' + hours;
+		}
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+
+		return hours + ':' + minutes + ':' + seconds;
+	}
+
+
+
+
+	// Configure the sortable
+	$scope.sortableOptions = {
+		handle: ".fa-reorder"
+	};
 }]);
 
-
-// App.directive('editInPlace', function () {
-// 	// Runs during compile
-// 	return {
-// 		restrict: 'E',
-// 		scope: { value: '=' },
-// 		template: '<span class="todoName" ng-dblclick="edit()" ng-bind="value" ng-class="{ \'completed-task\': todo.done }"></span><input class="todoField" ng-model="vaulue"></input>',
-// 		link: function($scope, iElm, iAttrs, controller) {
-// 			var inputElement = angular.element(iElm.children()[1]);
-
-// 			iElm.addClass('edit-in-place');
-
-// 			$scope.editing = false;
-
-// 			$scope.edit = function () {
-// 				$scope.editing = true;
-
-// 				iElm.addClass('active');
-
-// 				inputElement.focus();
-// 			};
-
-// 			inputElement.on('blur', function () {
-// 				$scope.editing = false;
-// 				iElm.removeClass('active');
-// 			});
-
-// 		}
-// 	};
-// });
-
-App.directive('ngEnter', function () {
-	// Runs during compile
+App.directive('ngEnter', function() {
 	return function (scope, elem) {
-		$(elem).keyup(function (event) {
+		$(elem).keyup(function(event) {
 			if (event.keyCode === 13) {
-				scope.$apply(function () {
+				scope.$apply(function() {
 					scope.addTask();
 				});
 			}
