8a0c6d04c50c548a7311ab883610bb4d28effd6a
Michael LoNigro <mvlonigro@gmail.com>
2015-01-07 23:28:22 -0700

    Added rest timer. Modified some formatting

--- js/app.js
+++ js/app.js
@@ -7,15 +7,20 @@ App.controller('toDoCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 	$scope.init = function() {
 		$scope.model = [{
 			'task': 'This is a test task',
-			'done': false
+			'done': false,
+			'time': 0
 		}, {
 			'task': 'This is another test task',
-			'done': false
+			'done': false,
+			'time': 0
 		}];
 
 		// Global time is stored in seconds
 		$scope.globalTimeNum = 0;
 		$scope.globalTime = formatTime($scope.globalTimeNum);
+
+		$scope.restTimeNum = 300;
+		$scope.restTime = formatTime($scope.restTimeNum);
 
 		$scope.show = "All";
 		$scope.currentShow = 0;
@@ -46,28 +51,71 @@ App.controller('toDoCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 
 	// Timer interface functions
 	$scope.startTimer = function() {
-		incrementTimer();
+		if (!_globalTimerRunning) {
+			incrementTimer();
+		}
 	};
 
 	$scope.stopTimer = function() {
 		$timeout.cancel($scope.timeout);
+		_globalTimerRunning = false;
 	};
 
 	$scope.resetTimer = function() {
+		$scope.stopTimer();
 		adjustTime(0);
 	};
 
+	$scope.startRest = function() {
+		if (!_restTimerRunning) {
+			decrementRest();
+		}
+	};
+
+	$scope.stopRest = function() {
+		$timeout.cancel($scope.restTimeout);
+		_restTimerRunning = false;
+	};
+
+	$scope.resetRest = function() {
+		$scope.stopRest();
+		adjustRest(300);
+	};
+
+	$scope.formatTime = function(time) {
+		formatTime(time);
+	}
+
 
 	// Timer helper functions
+	var _globalTimerRunning = false;
+	var _restTimerRunning = false;
+
 	function adjustTime(newTime) {
 		$scope.globalTimeNum = newTime;
 		$scope.globalTime = formatTime($scope.globalTimeNum);
 	}
 
 	function incrementTimer() {
+		_globalTimerRunning = true;
 		var newTime = $scope.globalTimeNum + 1;
 		adjustTime(newTime);
 		$scope.timeout = $timeout(incrementTimer, 1000);
+	}
+
+	function adjustRest(newTime) {
+		$scope.restTimeNum = newTime;
+		$scope.restTime = formatTime($scope.restTimeNum);
+	}
+
+	function decrementRest() {
+		if ($scope.restTimeNum == 0) {
+			return;
+		}
+		_restTimerRunning = true;
+		var newTime = $scope.restTimeNum - 1;
+		adjustRest(newTime);
+		$scope.restTimeout = $timeout(decrementRest, 1000);
 	}
 
 	function formatTime(time) {
@@ -89,9 +137,6 @@ App.controller('toDoCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 
 		return hours + ':' + minutes + ':' + seconds;
 	}
-
-
-
 
 	// Configure the sortable
 	$scope.sortableOptions = {
